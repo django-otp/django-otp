33683f1841db2b1cc7e72236fcc220d0bc697809
Peter Sagerson <psagers.hg@ignorare.net>
2016-01-10 13:53:25 -0800

    Minor releases.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-otp',
-    version='0.3.3',
+    version='0.3.4',
     description='A pluggable framework for adding two-factor authentication to Django using one-time passwords.',
     long_description=open('README.rst').read(),
     author='Peter Sagerson',
