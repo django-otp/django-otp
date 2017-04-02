1d6bbdb5a8b3736aa4324d4b714c1d0c25a3ef88
Peter Sagerson <psagers@ignorare.net>
2017-04-02 12:57:42 -0700

    django-otp version 0.3.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-otp',
-    version='0.3.11',
+    version='0.3.12',
     description='A pluggable framework for adding two-factor authentication to Django using one-time passwords.',
     long_description=open('README.rst').read(),
     author='Peter Sagerson',
