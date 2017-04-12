b7b38bbe6a2bba4b5af382ba4656e4a22d9bb335
nazarewk <nazarewk@localhost>
2017-04-11 17:44:17 -0700

    Enable pickling of `request.user` object (cannot pickle lambda)

--- django_otp/tests.py
+++ django_otp/tests.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import pickle
 from doctest import DocTestSuite
 
 import django
@@ -7,6 +8,8 @@ import django.test
 
 from django_otp import oath
 from django_otp import util
+from django_otp import DEVICE_ID_SESSION_KEY
+from django_otp.middleware import OTPMiddleware
 
 if django.VERSION < (1, 7):
     from django.utils import unittest
@@ -49,6 +52,31 @@ class TestCase(django.test.TestCase):
 
         This may raise an exception if the user model is too exotic for our
         purposes.
-
         """
         return self.User.objects.create_user(username, password=password)
+
+
+
+class OTPMiddlewareTestCase(TestCase):
+    def setUp(self):
+        self.factory = django.test.RequestFactory()
+        try:
+            user = self.create_user('alice', 'password')
+        except IntegrityError:
+            self.skipTest("Unable to create a test user.")
+        else:
+            device = user.staticdevice_set.create(id=1)
+            device.token_set.create(token='alice1')
+
+    def test_pickling(self):
+        middleware = OTPMiddleware()
+        request = self.factory.get('/')
+        request.user = user = self.User.objects.first()
+        device = user.staticdevice_set.first()
+        request.session = {
+            DEVICE_ID_SESSION_KEY: device.persistent_id
+        }
+        middleware.process_request(request)
+
+        pickle.dumps(request.user)
+
