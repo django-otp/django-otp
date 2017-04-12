7229f1fd443f70ba8405c90789f85c088005d2a7
Peter Sagerson <psagers@ignorare.net>
2017-04-11 17:44:53 -0700

    Fix and expand middleware tests.

--- django_otp/middleware.py
+++ django_otp/middleware.py
@@ -31,7 +31,6 @@ class OTPMiddleware(MiddlewareMixin):
     def process_request(self, request):
         user = getattr(request, 'user', None)
         if user is not None:
-            # Using simple lambda function prevents user object from getting pickled for example in celery
             request.user = SimpleLazyObject(functools.partial(self._verify_user, request, user))
 
         return None
@@ -40,9 +39,7 @@ class OTPMiddleware(MiddlewareMixin):
         """
         Sets OTP-related fields on an authenticated user.
         """
-
         user.otp_device = None
-        # Using simple lambda function prevents user object from getting pickled for example in celery
         user.is_verified = functools.partial(is_verified, user)
 
         if _user_is_authenticated(user):
