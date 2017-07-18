ff3a79936abf0622921538620e2aa0dfad92d1ff
Aaron Klein <ajmath62@gmail.com>
2017-07-18 11:37:40 -0400

    Modified addstatictoken.py to use argparse so that it works with Django>=1.10

--- django_otp/plugins/otp_static/management/commands/addstatictoken.py
+++ django_otp/plugins/otp_static/management/commands/addstatictoken.py
@@ -9,19 +9,16 @@ from django_otp.plugins.otp_static.lib import get_user_model, add_static_token
 
 
 class Command(BaseCommand):
-    option_list = getattr(BaseCommand, 'option_list', ()) + (
-        make_option('-t', '--token', dest='token', help='The token to add. If omitted, one will be randomly generated.'),
-    )
-    args = '<username>'
     help = fill('Adds a single static OTP token to the given user. '
                 'The token will be added to an arbitrary static device '
                 'attached to the user, creating one if necessary.', width=78)
 
-    def handle(self, *args, **options):
-        if len(args) != 1:
-            raise CommandError('Please specify exactly one username.')
+    def add_arguments(self, parser):
+        parser.add_argument('-t', '--token', dest='token', help='The token to add. If omitted, one will be randomly generated.')
+        parser.add_argument('username', help='The user to which the token will be assigned.')
 
-        username = args[0]
+    def handle(self, *args, **options):
+        username = options['username']
 
         try:
             statictoken = add_static_token(username, options.get('token'))
