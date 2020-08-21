40601d6c395984a17fb893da37cbf46920358f39
Peter Sagerson <psagers@ignorare.net>
2020-08-21 11:06:58 -0700

    Clean up package data configuration.

--- setup.py
+++ setup.py
@@ -1,40 +1,6 @@
 #!/usr/bin/env python
 
-import os
-import os.path
-import re
-
 from setuptools import find_packages, setup
-
-
-def gen_package_data(pkg_root, paths, prune=[]):
-    """
-    Generates a value for package_data.
-
-    pkg_root is the path to the Python package we're generating package_data
-    for. paths is a list of paths relative to pkg_root to add. We'll search
-    these directories recursively, yielding a sequence of '<sub-path>/*'
-    strings to select every nested file for inclusion.
-
-    The optional third argument is a collection of directory names to prune
-    from the traversal.
-
-    """
-    pkg_root = os.path.abspath(pkg_root)
-
-    # For stripping pkg_root from results.
-    root_re = re.compile(r'^' + re.escape(pkg_root) + r'/*')
-
-    for path in paths:
-        for dirpath, dirnames, _ in os.walk(os.path.join(pkg_root, path)):
-            dirpath = root_re.sub('', dirpath)
-            yield os.path.join(dirpath, '*')
-            if prune:
-                dirnames[:] = [d for d in dirnames if d not in prune]
-
-
-def find_package_data(*args, **kwargs):
-    return list(gen_package_data(*args, **kwargs))
 
 
 setup(
@@ -62,12 +28,7 @@ setup(
 
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    package_data={
-        'django_otp': find_package_data('src/django_otp', ['templates']),
-        'django_otp.plugins.otp_email': find_package_data('src/django_otp/plugins/otp_email', ['templates']),
-        'django_otp.plugins.otp_hotp': find_package_data('src/django_otp/plugins/otp_hotp', ['templates']),
-        'django_otp.plugins.otp_totp': find_package_data('src/django_otp/plugins/otp_totp', ['templates']),
-    },
+    include_package_data=True,
     zip_safe=False,
 
     install_requires=[
