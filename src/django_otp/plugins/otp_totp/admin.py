15984988c133f5fad4a171e60d61d3b3b99a0705
Petr Dlouhý <petr.dlouhy@email.cz>
2023-02-10 17:10:19 +0100

    allow to search tokens for TOTP plugin

--- src/django_otp/plugins/otp_totp/admin.py
+++ src/django_otp/plugins/otp_totp/admin.py
@@ -5,10 +5,14 @@ from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from django.urls import path, reverse
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
 
 from django_otp.conf import settings
 
 from .models import TOTPDevice
+
+
+User = get_user_model()
 
 
 class TOTPDeviceAdmin(admin.ModelAdmin):
@@ -17,6 +21,9 @@ class TOTPDeviceAdmin(admin.ModelAdmin):
     :class:`~django_otp.plugins.otp_totp.models.TOTPDevice`.
     """
     list_display = ['user', 'name', 'confirmed']
+
+    def get_search_fields(self, request):
+        return [f'user__{User.get_email_field_name()}', f'user__{User.USERNAME_FIELD}', 'name']
 
     raw_id_fields = ['user']
     readonly_fields = ['qrcode_link']
