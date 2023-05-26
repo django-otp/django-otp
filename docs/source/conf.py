ac0cbaba83cae72088030756b584bc5da8a5fd60
Peter Sagerson <psagers@ignorare.net>
2023-05-26 06:34:36 -0700

    Version 1.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,7 @@ project = 'django-otp'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '1.2.0'
+release = '1.2.1'
 
 # The short X.Y version.
 version = '.'.join(release.split('.')[:2])
